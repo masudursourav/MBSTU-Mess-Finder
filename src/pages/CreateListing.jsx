9bacf98e328cc3a0f4198ca60c031c60e3d08227
masudursourav <masudursourav@ieee.org>
2023-08-30 08:29:25 +0600

    Listing page added

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -3,7 +3,9 @@ import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { v4 as uuidv4 } from "uuid";
-
+import {getStorage, ref, uploadBytesResumable,getDownloadURL} from "firebase/storage"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase.config";
 const CreateListing = () => {
     const [loading,setLoading] = useState(true)
     const [formData,setFormData] = useState({
@@ -17,7 +19,7 @@ const CreateListing = () => {
         offer:false,
         regularPrice:0,
         discountedPrice:0,
-        images:{},
+        images:[],
         Phone:'',
 
     })
@@ -43,7 +45,7 @@ const CreateListing = () => {
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[isMounted])
-    const onSubmit = (e)=>{
+    const onSubmit =async (e)=>{
         e.preventDefault()
         setLoading(true)
         if(discountedPrice >= regularPrice){
@@ -51,11 +53,69 @@ const CreateListing = () => {
             toast.error('Discounted price cannot be greater then the regular price')
             return
         }
-        if(images.length){
+        if(images.length>6){
             setLoading(false)
             toast.error('The maximum number of images is 6')
         }
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+              const storage = getStorage()
+              const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+      
+              const storageRef = ref(storage, 'images/' + fileName)
+      
+              const uploadTask = uploadBytesResumable(storageRef, image)
+      
+              uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                  const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                  console.log('Upload is ' + progress + '% done')
+                  switch (snapshot.state) {
+                    case 'paused':
+                      console.log('Upload is paused')
+                      break
+                    case 'running':
+                      console.log('Upload is running')
+                      break
+                    default:
+                      break
+                  }
+                },
+                (error) => {
+                  reject(error)
+                },
+                () => {
+                  getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL)
+                  })
+                }
+              )
+            })
+          }
+      
+          const imageUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+          ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+          })
 
+          const formDataCopy = {
+            ...formData,
+            imageUrls,
+            timestamp: serverTimestamp()
+          }
+          delete formDataCopy.images
+          formDataCopy.location = address
+          !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+          const docRef = await addDoc(collection(db,'listing'),formDataCopy)
+          setLoading(false)
+          toast.success('Listing Saved')
+          navigate(`/category/${formDataCopy.type}/${docRef.id}`)
     }
     const onMutate = (e) =>{
         let boolean = null
@@ -78,6 +138,8 @@ const CreateListing = () => {
             }))
         }
     }
+
+     
     return (
         <>
            <div className="profile">
